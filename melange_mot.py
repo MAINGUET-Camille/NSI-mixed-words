7f1834cf2d329d04c54d2edbcd4c09b305f91ac2
MAINGUET-Camille <103934712+MAINGUET-Camille@users.noreply.github.com>
2022-10-23 17:53:06 +0200

    amelioration de melange_mot()

--- melange_mot.py
+++ melange_mot.py
@@ -3,7 +3,7 @@
 from random import randrange
 
 #fonction pour mélanger un mot
-def melange_mot(mot:str):
+def melange_mot(mot:str) -> str:
     """
     entrée: un mot (str)
     sortie: le mot mélangé (la première et dernière lettre du mot ne change pas de place.)
@@ -11,26 +11,18 @@ def melange_mot(mot:str):
     if len(mot) <= 3:   #Si le mot a trois ou moins de lettres, le mélange ne change rien
         return mot
 
-    #prend les premières et dernières lettres du mot et les retire
-    P = mot[0]
-    mot = mot[1:]
-    D = mot[-1]
-    mot = mot[:-1]
-
-    T=[]
+    T = []
     for LETTRE in mot:
-        T.append(LETTRE)    #crée un tableau contenant chaque lettre du mot
-
-    #mélange les lettres contenues dans le tableau T
-    for i in range(len(T),1,-1): #i tourne les indexes possibles des différents charactères du mot dans l'ordre décroissant
-        aleat = randrange(i-1)
-        T[i-1], T[aleat] = T[aleat], T[i-1]
-
+        T.append(LETTRE)
+    
     mot = ""
-    for LETTRE in T:
-        mot += LETTRE    #remet toutes les lettres du tableau T dans un même string
+    for i in range(len(T)-2):
+        aleat = randrange(1,len(T)-1) #permet d'obtenir l'indice d'une lettre aléatoirement, ne prends pas en compte le premier et dernier élément du tableau.
+        mot += T.pop(aleat)
 
-    return P + mot + D 
+
+    return T[0] + mot + T[1] #remet les premières et dernières lettres qui étaient restées dans le tableau à leur place.
+
 
 
 
