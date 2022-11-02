8b87d18aa278268e966eaa38d9eeae62604e74b4
MAINGUET-Camille <103934712+MAINGUET-Camille@users.noreply.github.com>
2022-11-02 11:46:14 +0100

    modif test

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from trier_un_mot import tri_mot
-from melange_mot import melange_mot
+from tri_phrase import tri_phrase
 
-x = "Boomerang"
-y = melange_mot(x)
-# print(x.lower())
-# print(x.istitle())
+x = """Sleon une édtue de l'Uvinertisé de Cmabrigde, l'odrre des 
+ltteers dans un mtos n'a pas d'ipmrotncae, la suele coshe 
+ipmrotnate est que la pmeirère et la drenèire soit à la bnnoe 
+pclae. Le rsete peut êrte dans un dsérorde ttoal et vuos puoevz 
+tujoruos lrie snas porlblème. C'est prace que le creaveu 
+hmauin ne lit pas chuaqe ltetre elle-mmêe mias le mot comme 
+un tuot."""
 
-print(y)
-print(tri_mot(y))
+print(tri_phrase(x))
