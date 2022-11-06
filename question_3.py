29b59b0a89519efcb40750c0700163fd085b95ba
MAINGUET-Camille <103934712+MAINGUET-Camille@users.noreply.github.com>
2022-11-06 17:58:21 +0100

    update de la lecture de fichier

--- question_3.py
+++ question_3.py
@@ -1,81 +1,31 @@
 # Adapter la fonction précédente pour qu’elle prenne en paramètre : 
 # le nom de fichier au format txt à transformer et le nom du fichier dans lequel ce texte transformé sera stocké
 
-from trier_un_mot import tri_mot
-import re as re
-
-def tri_phrase(phrase: str) -> str:
-    """
-    entrée : une ou plusieurs phrases (str) 
-    sortie : la/les phrase(s) avec chaque mot mélangé (la première et dernière lettre du mot ne change pas de place)
-    """
-
-    # séparation des mots et des marques de ponctuation
-    T_mots = re.findall("\w+", phrase)
-    T_ponctuation = re.findall("\W+", phrase)
-
-    T_mots_tries = [] #création d'un tableau pour stocker les mots une fois triés
-   
-    for mot in T_mots:
-        #le dictionnaire ne comporte pas de mots avec des majuscules, il faut les enlever
-        if not mot.islower():
-
-            #le mot peut avoir une majuscule au début (title) ou être entièrement compris de majuscules (upper)
-            if mot.istitle():
-                #on tri le mot et on remet la majuscule
-                mot = mot.lower()
-                mot_trie = tri_mot(mot)
-                mot_trie = mot_trie.capitalize()
-            elif mot.isupper():
-                #on tri le mot et on le remet en majuscules
-                mot = mot.lower()
-                mot_trie = tri_mot(mot)
-                mot_trie = mot_trie.upper()
-        
-        else:
-            mot_trie = tri_mot(mot)
-
-        #on ajoute le mot trié au tableau de mots triés.
-        T_mots_tries.append(mot_trie)
-
-    #si les deux tableaux ne sont pas de la même taille, c'est qu'il manque un point à la fin de la phrase.
-    if len(T_mots) != len(T_ponctuation):
-        T_ponctuation.append(".")
-
-    #on remet les mots et la ponctuation ensemble pour reformer la phrase dans phrase_trie
-    phrase_trie = ""
-    for i in range(len(T_mots)):
-        phrase_trie += T_mots_tries[i]
-        phrase_trie += T_ponctuation[i]
-
-    return phrase_trie
-
+from fonction_melange_phrase import melange_phrase
 
 def question_3(tout_transformer, stockage):
     '''
-    Entrée: un fichier texte contenant le texte à transformer
+    Entrée: un fichier texte contenant le texte à transformer (tout_transformer) et le nom sous lequel le nouveau fichier va être crée (stockage)
     Fonction qui applique les règles de mélange au texte et le stock dans un autre fichier
     '''
-    try:
-        t = open(tout_transformer,'r', encoding='utf-8') #ouverture du fichier en lecture seule sous le codage utf-8 (présence d'accents)
-        #lecture du fichier
-        fichier_de_sortie = t.read()
-        #print(fichier_de_sortie)
-        #fermeture du fichier
-        t.close()
 
-        #transformation du texte
-        chaine_triee = tri_phrase(fichier_de_sortie)
+    t = open(tout_transformer,'r', encoding='utf-8') #ouverture du fichier en lecture seule sous le codage utf-8 (présence d'accents)
+    #lecture du fichier
+    fichier_de_sortie = t.read()
+    #print(fichier_de_sortie)
+    #fermeture du fichier
+    t.close()
 
-        #ouverture du fichier de sortie et écriture du texte transformé
-        file = open(stockage, 'w')
-        #écriture du texte transformé dans le fichier de sortie
-        file.write(chaine_triee)
-        #fermeture le fichier
-        file.close()
-    except IOError:
-        print ("Erreur: Erreur d'ouverture du fichier")
+    #transformation du texte
+    chaine_triee = melange_phrase(fichier_de_sortie)
+
+    #ouverture du fichier de sortie et écriture du texte transformé
+    file = open(stockage, 'w', encoding="utf-8")
+    #écriture du texte transformé dans le fichier de sortie
+    file.write(chaine_triee)
+    #fermeture le fichier
+    file.close()
     return
 
-#on définit en paramètre les chemins relatifs qui mènent vers les fichiers
-question_3("C:\\Users\\utilisateur\\Desktop\\NSI\\NSI-mixed-words\\tout_transformer.txt", "C:\\Users\\utilisateur\\Desktop\\NSI\\NSI-mixed-words\\stockage.txt")
+# #on définit en paramètre les chemins relatifs qui mènent vers les fichiers
+# question_3("C:\\Users\\utilisateur\\Desktop\\NSI\\NSI-mixed-words\\tout_transformer.txt", "C:\\Users\\utilisateur\\Desktop\\NSI\\NSI-mixed-words\\stockage.txt")
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 from tri_phrase import tri_phrase
 from fonction_melange_phrase import melange_phrase
+from question_3 import question_3
 
 phrase_1 = """Selon une étude de l'Université de Cambridge, l'ordre des lettres dans les mots n'a pas d'importance, la seule chose importante est que la première et la dernière soit à la bonne place. 
 Le reste peut être dans un désordre total et vous pouvez toujours lire sans problème. 
 C'est parce que le cerveau humain ne lit pas chaque lettre elle-même mais le mot comme un tout."""
+
+#print(melange_phrase(phrase_1))
 
 phrase_2 = """Sleon une édtue de l'Uvinertisé de Cmabrigde, l'odrre des 
 ltteers dans un mtos n'a pas d'ipmrotncae, la suele coshe 
@@ -13,6 +16,6 @@ tujoruos lrie snas porlblème. C'est prace que le creaveu
 hmauin ne lit pas chuaqe ltetre elle-mmêe mias le mot comme 
 un tuot."""
 
+# print(tri_phrase(phrase_2))
 
-print(melange_phrase(phrase_1))
-print(tri_phrase(phrase_2))
+question_3("phrase.txt","phrase_melange.txt")
