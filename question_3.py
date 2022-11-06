06d81fbf478996d6e388709f9816c2c1925b98b6
eloise15 <116354895+eloise15@users.noreply.github.com>
2022-11-06 17:13:33 +0000

    Update question_3.py

--- question_3.py
+++ question_3.py
@@ -12,20 +12,16 @@ def question_3(tout_transformer, stockage):
     t = open(tout_transformer,'r', encoding='utf-8') #ouverture du fichier en lecture seule sous le codage utf-8 (présence d'accents)
     #lecture du fichier
     fichier_de_sortie = t.read()
-    #print(fichier_de_sortie)
     #fermeture du fichier
     t.close()
 
     #transformation du texte
     chaine_triee = melange_phrase(fichier_de_sortie)
 
-    #ouverture du fichier de sortie et écriture du texte transformé
+    #ouverture du fichier de sortie et écriture du texte transformé sous le codage utf-8
     file = open(stockage, 'w', encoding="utf-8")
     #écriture du texte transformé dans le fichier de sortie
     file.write(chaine_triee)
     #fermeture du fichier
     file.close()
     return
-
-# #on définit en paramètre les chemins relatifs qui mènent vers les fichiers
-# question_3("C:\\Users\\utilisateur\\Desktop\\NSI\\NSI-mixed-words\\tout_transformer.txt", "C:\\Users\\utilisateur\\Desktop\\NSI\\NSI-mixed-words\\stockage.txt")
