a30c56b026b0243254c9f04ee58f0543bf3472bf
MAINGUET-Camille <103934712+MAINGUET-Camille@users.noreply.github.com>
2022-11-06 14:24:17 +0100

    update melange_phrase

--- fonction_melange_phrase.py
+++ fonction_melange_phrase.py
@@ -5,25 +5,16 @@ def melange_phrase(phrase):
        sortie : la/les phrase(s) avec chaque mot mélangé (la première
        et dernière lettre du mot ne change pas de place)"""
 
-    T=phrase.split()  # création d'un tableau qui contient chaque mots de la phrase grâce à la fonction split()
+    import re  #importation du module re
+    T=re.split(r'(\W+)',phrase) #création d'un tableau grâce à la fonction split() avec comme séparateur tout caractère non alphanumérique
 
-    phrase_melange=""  #variable vide pour stocker la phrase mélangée
+    phrase_melange="" #variable vide dans laquelle on mettra la phrase melangée
 
     for i in T:
-        if i[-1] == ",":
-            i=i[:-1]
-            i=melange_mot(i) + ","
-            phrase_melange+=i + " " # si le mot se termine par une virgule, on enleve la virgule, on mélange le mot, on rajoute la vrigule et on ajoute le mot dans la variable phrase_melange
-        elif i[-1] == ".":
-            i=i[:-1]
-            i=melange_mot(i) + "."
-            phrase_melange+=i + " "  # si le mot se termine par un point, on enleve le point, on mélange le mot, on rajoute le point et on ajoute le mot dans la variable phrase_melange
-        else:
-            i=melange_mot(i)
-            phrase_melange+=i + " " # ajout dans la variable "phrase_melange" les autres éléments du tableau "T", en appliquant à chaque éléments la fonction "melange_mot" et on met un espace entre chaque mot
+        phrase_melange+=melange_mot(i) #pour chaque élément du tableau, on applique la fonction melange_mot() et on l'ajoute dans la variable phrase_melange
 
-    return phrase_melange
+    return(phrase_melange)
 
-#exemple
-phrase="Selon une étude de l'Université de Cambridge, l'ordre des lettres dans les mots n'a pas d'importance, la seule chose importante est que la première et la dernière soit à la bonne place. Le reste peut être dans un désordre total et vous pouvez toujours lire sans problème. C'est parceque le cerveau humain ne lit pas chaque lettres elle-même mais le mot comme un tout."
-print(melange_phrase(phrase))
+# #exemple
+# phrase="Selon une étude de l'Université de Cambridge, l'ordre des lettres dans les mots n'a pas d'importance, la seule chose importante est que la première et la dernière soit à la bonne place. Le reste peut être dans un désordre total et vous pouvez toujours lire sans problème. C'est parceque le cerveau humain ne lit pas chaque lettres elle-même mais le mot comme un tout."
+# print(melange_phrase(phrase))
--- test.py
+++ test.py
@@ -3,9 +3,9 @@ Le reste peut être dans un désordre total et vous pouvez toujours lire sans pr
 C'est parce que le cerveau humain ne lit pas chaque lettre elle-même mais le mot comme un tout."""
 
 
-from separer_les_mots import melange_phrase2
+from fonction_melange_phrase import melange_phrase
 
-print(melange_phrase2(phrase))
+print(melange_phrase(phrase))
 
 
 
